889c75d98ee3c75b0a4667bf2f7d2679edae05f8
nik-vitaly <nik.vitaly@mail.ru>
2016-02-18 00:07:28 +0300

    add loader

--- app/src/main/java/net/nikonorov/videolenta/ActivityMain.java
+++ app/src/main/java/net/nikonorov/videolenta/ActivityMain.java
@@ -1,31 +1,27 @@
 package net.nikonorov.videolenta;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 
-import net.nikonorov.videolenta.logic.LentaService;
+import net.nikonorov.videolenta.logic.RowLoader;
 
 import java.util.ArrayList;
-import java.util.List;
 
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 
 /**
  * Created by vitaly on 16.02.16.
  */
-public class ActivityMain extends AppCompatActivity {
+public class ActivityMain extends AppCompatActivity implements LoaderManager.LoaderCallbacks<PostList> {
 
     private RecyclerView recyclerView;
     private RVAdapter adapter;
     private ArrayList<Article> data;
+
+    private final static int LOADER_ID = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -35,28 +31,40 @@ public class ActivityMain extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.rv_lenta);
         recyclerView.setLayoutManager(new LinearLayoutManager(ActivityMain.this));
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://nikonorov.net")
-                .addConverterFactory(SimpleXmlConverterFactory.create())
-                .build();
+        data = new ArrayList<Article>(new ArrayList<Article>());
 
-        LentaService myAPI = retrofit.create(LentaService.class);
+        adapter = new RVAdapter(data, ActivityMain.this);
+        recyclerView.setAdapter(adapter);
+        getLoaderManager().initLoader(1, Bundle.EMPTY, ActivityMain.this);
+    }
 
-        Call<PostList> call = myAPI.getPosts();
+    @Override
+    public Loader<PostList> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case LOADER_ID:
+                return new RowLoader(ActivityMain.this);
+            default:
+                return null;
+        }
+    }
 
-        call.enqueue(new Callback<PostList>() {
+    @Override
+    public void onLoadFinished(Loader<PostList> loader, PostList list) {
+        data.clear();
+        data.addAll(list.getArticleList());
+        runOnUiThread(new Runnable() {
             @Override
-            public void onResponse(retrofit2.Call<PostList> call, Response<PostList> response) {
-                data = new ArrayList<Article>(response.body().getArticleList());
-
-                adapter = new RVAdapter(data, ActivityMain.this);
-                recyclerView.setAdapter(adapter);
-            }
-
-            @Override
-            public void onFailure(retrofit2.Call<PostList> call, Throwable t) {
-
+            public void run() {
+                adapter.notifyDataSetChanged();
             }
         });
+
+        getLoaderManager().destroyLoader(LOADER_ID);
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<PostList> loader) {
+
     }
 }
---
+++ app/src/main/java/net/nikonorov/videolenta/logic/RowLoader.java
@@ -0,0 +1,72 @@
+package net.nikonorov.videolenta.logic;
+
+import android.content.Context;
+import android.content.Loader;
+
+import net.nikonorov.videolenta.PostList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
+
+/**
+ * Created by vitaly on 18.02.16.
+ */
+public class RowLoader extends Loader<PostList> {
+
+    private PostList data;
+    private Retrofit retrofit;
+
+    public RowLoader(Context context) {
+        super(context);
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://nikonorov.net")
+                .addConverterFactory(SimpleXmlConverterFactory.create())
+                .build();
+    }
+
+    @Override
+    public void deliverResult(PostList data) {
+        super.deliverResult(data);
+
+        this.data = data;
+    }
+
+    @Override
+    protected void onStartLoading() {
+        if (data != null) {
+            deliverResult(data);
+        } else {
+            forceLoad();
+        }
+    }
+
+    @Override
+    protected void onForceLoad() {
+
+        LentaService myAPI = retrofit.create(LentaService.class);
+
+        Call<PostList> call = myAPI.getPosts();
+
+        call.enqueue(new Callback<PostList>() {
+            @Override
+            public void onResponse(retrofit2.Call<PostList> call, Response<PostList> response) {
+                if(response.isSuccess()) {
+                    deliverResult(response.body());
+                }
+            }
+
+            @Override
+            public void onFailure(retrofit2.Call<PostList> call, Throwable t) {
+                try {
+                    throw t;
+                } catch (Throwable throwable) {
+                    throwable.printStackTrace();
+                }
+            }
+        });
+    }
+}
